91837465b1589d61f19bce724ccdf208269f935a
vivek-techi <79888528+vivek-techi@users.noreply.github.com>
2021-03-12 10:47:29 +0530

    Add files via upload

---
+++ Strnglength.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Simplejava {
+	
+	
+		public static void main(String[] args) {
+	int i=0;
+	String str;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the string");
+	str=sc.nextLine();
+	int n=str.length();
+	System.out.println("length of the string:" +n);
+		}
+	}
+
+
